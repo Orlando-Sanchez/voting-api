0b9c7a9867372b9e2bdb457bca6407ec950d28e8
orlando <or.sanchez100@gmail.com>
2022-05-07 01:17:18 -0400

    Create poll model, routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :polls, only: [:index, :show, :create, :destroy]
+    end
+  end
 end
