4a02af9f7859c5a90448fc0aeebe0064d5c6c513
orlando <or.sanchez100@gmail.com>
2022-06-04 22:17:16 -0400

    Add poll option model tests

---
+++ spec/models/poll_option_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe PollOption do
+	it { is_expected.to belong_to(:poll) }
+
+	it { is_expected.to validate_presence_of(:title) }
+	it { is_expected.to validate_length_of(:title).is_at_least(1) }
+end
--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Poll do
-  it { is_expected.to have_many(:poll_options) }
+	it { is_expected.to have_many(:poll_options) }
 
-  it { is_expected.to validate_presence_of(:subject) }
-  it { is_expected.to validate_length_of(:subject).is_at_least(3) }
+	it { is_expected.to accept_nested_attributes_for(:poll_options)}
 
-  it { is_expected.to validate_presence_of(:poll_options) }
+	it { is_expected.to validate_presence_of(:subject) }
+	it { is_expected.to validate_length_of(:subject).is_at_least(3) }
+
+	it { is_expected.to validate_presence_of(:poll_options) }
 end
