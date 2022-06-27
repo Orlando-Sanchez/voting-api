6bb7a2f54a993a5332c72d5cd0097a5dfaa8fd0f
orlando <or.sanchez100@gmail.com>
2022-06-27 15:22:06 -0400

    Add before actions to vote create and increased jwt valid exp time

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -1,12 +1,13 @@
 module Api
   module V1
     class VotesController < ApplicationController
-
-      before_action :poll
+      
+      before_action :authenticate_user!
+      before_action :current_user_voted?
 
       def create
         vote = current_user.votes.build(
-          :poll_id => @_poll.id
+          :poll_id => @poll.id
         )
 
         if vote.save
@@ -17,9 +18,13 @@ module Api
       end
 
       private
+    
+      def current_user_voted?
+        @poll = Poll.find(params[:poll_id])
 
-      def poll
-        @_poll = Poll.find(params[:poll_id])
+        if @poll.votes.where(user_id: current_user.id).exists?
+          render json: { message: 'User already voted in this poll.' }, status: :unprocessable_entity
+        end
       end
     end
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,7 +25,7 @@ Devise.setup do |config|
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 120.minutes.to_i
+    jwt.expiration_time = 180.minutes.to_i
   end
 
   # ==> Controller configuration
