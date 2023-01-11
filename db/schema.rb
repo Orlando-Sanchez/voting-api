fab778a7bd4951dcb877da1c36750edbc5d679c2
Orlando <or.sanchez100@gmail.com>
2023-01-11 19:43:50 -0400

    fix: remove ballots count from poll options

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_01_224345) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_11_233535) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_01_224345) do
     t.bigint "poll_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "ballots_count"
     t.index ["poll_id"], name: "index_poll_options_on_poll_id"
   end
 
